d3db0635a610274816a9ce915e479ce0f3cfda2a
alnhdgrlla <y.tanaka@shippio.io>
2019-11-15 13:44:07 +0900

    delete manager added

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,6 +5,7 @@ module Types
     field :create_spam, mutation: Mutations::CreateSpam
     field :create_template, mutation: Mutations::CreateTemplate
     field :create_subscriber, mutation: Mutations::CreateSubscriber
+    field :delete_manager, mutation: Mutations::DeleteManager
     field :delete_spam, mutation: Mutations::DeleteSpam
     field :delete_template, mutation: Mutations::DeleteTemplate
     field :delete_subscriber, mutation: Mutations::DeleteSubscriber
