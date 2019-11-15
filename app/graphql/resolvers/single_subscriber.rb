769c8ee1a089d838ada686443b44bc1524755d1a
alnhdgrlla <y.tanaka@shippio.io>
2019-11-15 14:47:16 +0900

    added sigle subscriber

--- app/graphql/resolvers/single_subscriber.rb
+++ app/graphql/resolvers/single_subscriber.rb
@@ -1,5 +1,5 @@
 module Resolvers
-  class SingleSpam < Resolvers::Base
+  class SingleSubscriber < Resolvers::Base
     argument :sub_id, ID, required: true
     argument :limit, Integer, required: false
     argument :offset, Integer, required: false
@@ -12,11 +12,11 @@ module Resolvers
       return GraphQL::ExecutionError.new("You have to log in") if !c_user
 
       sub = Subscriber.find_by(id: sub_id)
-      return GraphQL::ExecutionError.new("There is not spam with that ID") if !sub
+      return GraphQL::ExecutionError.new("There is not subscriber with that ID") if !sub
       
-      return GraphQL::ExecutionError.new("You do not have the right to see this subscriber") if !c_user.id = sub.manager_id
+      return GraphQL::ExecutionError.new("You do not have the right to see this subscriber") if !c_user.id = sub.spam.manager_id
       
-      spam
+      sub 
     end
   end
 end
